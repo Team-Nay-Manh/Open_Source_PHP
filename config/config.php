1c96e91870dfbd369e74335576154527eb76666e
Pham Tran Nhat Minh <nhatminh4403@gmail.com>
2025-03-19 18:16:55 +0700

    (OSP-39): css & config for chatbot

--- config/config.php
+++ config/config.php
@@ -9,7 +9,9 @@ define('MYSQL_USER', 'root');
 define('MYSQL_PASS', '');
 define('MYSQL_DB', 'da_php');
 define('DATA_PER_PAGE', '9');
-
-
+define('DIALOGFLOW_AGENT_ID','6e08a620-4272-4ba0-be06-c4f3b7366ddf');
+define('DIALOGFLOW_LANGUAGE_CODE','vi');
+define('DIALOGFLOW_CHAT_TITLE','Vì Tinh Tú - Chatbot');
+define('DIALOGFLOW_CHAT_ICON','https://cdn-icons-png.flaticon.com/512/4954/4954813.png');
 include_once("function.php");
 include_once("DB.php");

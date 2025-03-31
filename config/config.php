4caa2b1d0af9f975c63d39e7db3ec4d13b121d47
sichehoang <165777956+sichehoang@users.noreply.github.com>
2025-03-31 16:15:06 +0700

    docker

--- config/config.php
+++ config/config.php
@@ -4,12 +4,13 @@
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 session_start();
 
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'root');
-define('MYSQL_PASS', '');
-define('MYSQL_DB', 'da_php');
-define('DATA_PER_PAGE', '9');
+define('MYSQL_HOST', 'mysql');  // Đổi từ 'localhost' thành 'mysql'
+define('MYSQL_USER', 'admin');   // Tên user trong docker-compose.yml
+define('MYSQL_PASS', 'admin'); // Mật khẩu đã đặt trong docker-compose.yml
+define('MYSQL_DB', 'da_php');   // Tên database
 
+define('DATA_PER_PAGE', '9');
 
 include_once("function.php");
 include_once("DB.php");
+?>

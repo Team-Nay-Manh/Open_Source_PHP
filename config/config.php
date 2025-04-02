6d0f740efc7599a11c5ab679bc330d196ca7d679
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-04-03 00:38:56 +0700

    fix(config): Connect DB

--- config/config.php
+++ config/config.php
@@ -4,16 +4,15 @@
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 session_start();
 
-define('MYSQL_HOST', 'mysql');  // Đổi từ 'localhost' thành 'mysql'
-define('MYSQL_USER', 'admin');   // Tên user trong docker-compose.yml
-define('MYSQL_PASS', 'admin'); // Mật khẩu đã đặt trong docker-compose.yml
+define('MYSQL_HOST', 'localhost');  // Đổi từ 'localhost' thành 'mysql'
+define('MYSQL_USER', 'root');   // Tên user trong docker-compose.yml
+define('MYSQL_PASS', ''); // Mật khẩu đã đặt trong docker-compose.yml
 define('MYSQL_DB', 'da_php');
 define('DATA_PER_PAGE', '9');
-define('DIALOGFLOW_AGENT_ID','6e08a620-4272-4ba0-be06-c4f3b7366ddf');
-define('DIALOGFLOW_LANGUAGE_CODE','vi');
-define('DIALOGFLOW_CHAT_TITLE','Vì Tinh Tú - Chatbot');
-define('DIALOGFLOW_CHAT_ICON','https://cdn-icons-png.flaticon.com/512/4954/4954813.png');
+define('DIALOGFLOW_AGENT_ID', '6e08a620-4272-4ba0-be06-c4f3b7366ddf');
+define('DIALOGFLOW_LANGUAGE_CODE', 'vi');
+define('DIALOGFLOW_CHAT_TITLE', 'Vì Tinh Tú - Chatbot');
+define('DIALOGFLOW_CHAT_ICON', 'https://cdn-icons-png.flaticon.com/512/4954/4954813.png');
 
 include_once("function.php");
 include_once("DB.php");
-?>
